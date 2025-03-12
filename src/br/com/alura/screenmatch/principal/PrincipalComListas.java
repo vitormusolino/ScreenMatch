d14836c1c683ffb75ce15496fbd43c308d69a444
Vitor Musolino Teixeira <160746719+vitormusolino@users.noreply.github.com>
2025-03-12 08:35:58 -0300

    Add files via upload

--- src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -4,9 +4,7 @@ import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 import br.com.alura.screenmatch.modelos.Titulo;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.*;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
@@ -18,7 +16,7 @@ public class PrincipalComListas {
         outroFilme.avalia(8);
         Serie lost = new Serie("Lost",2007);
 
-        ArrayList<Titulo> listaAssistidos = new ArrayList<>();
+        List<Titulo> listaAssistidos = new LinkedList<>();
         listaAssistidos.add(filmeDoPaulo);
         listaAssistidos.add(meuFilme);
         listaAssistidos.add(outroFilme);
